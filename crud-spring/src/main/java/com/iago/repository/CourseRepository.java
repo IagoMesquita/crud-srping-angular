46807c0d33bdf3ba7ace6751056f2d307637fedb
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-04 08:19:41 -0300

    chore: add repository course

---
+++ crud-spring/src/main/java/com/iago/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.iago.repository;
+
+import com.iago.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
