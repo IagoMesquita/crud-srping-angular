e6f59396853b6face012c618ee6c73a7a71e9709
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-05 08:54:30 -0300

    feat: add seeds in sql import

---
+++ crud-spring/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Query: SELECT * FROM a
+-- exported data
+INSERT INTO `courses` (`id`,`name`,`category`,`status`) VALUES (1,'Java','Back-end', 'Ativo');
+INSERT INTO `courses` (`id`,`name`,`category`,`status`) VALUES (2,'Spring','Back-end','Ativo');
