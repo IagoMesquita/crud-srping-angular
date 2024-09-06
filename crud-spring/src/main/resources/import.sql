82546e944142829e30a8e484ff91ab8ff1b4987e
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-06 08:25:39 -0300

    refactor: seed  for db h2

--- crud-spring/src/main/resources/import.sql
+++ crud-spring/src/main/resources/import.sql
@@ -2,3 +2,4 @@
 -- exported data
 INSERT INTO `courses` (`id`,`name`,`category`,`status`) VALUES (1,'Java','Back-end', 'Ativo');
 INSERT INTO `courses` (`id`,`name`,`category`,`status`) VALUES (2,'Spring','Back-end','Ativo');
+INSERT INTO `courses` (`id`,`name`,`category`,`status`) VALUES (3,'Flutter','Front-end','Ativo');
