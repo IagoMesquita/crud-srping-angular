7bfc6cd20e2d39bc4afbdfa127d5e4e493648928
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-10 11:54:56 -0300

    feat: add mapper to course

---
+++ crud-spring/src/main/java/com/iago/dto/mapper/CourseMapper.java
@@ -0,0 +1,37 @@
+package com.iago.dto.mapper;
+
+import com.iago.dto.CourseDTO;
+import com.iago.model.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+
+  public CourseDTO toDTO(Course course) {
+    if (course == null) { // evitar o nullpointExcepiton
+      return null;
+    }
+    return new CourseDTO(
+        course.getId(),
+        course.getName(),
+        course.getCategory()
+    );
+  }
+
+  public Course toEntity(CourseDTO courseDTO) {
+    if(courseDTO == null) {
+      return null;
+    }
+
+    Course course = new Course();
+
+    if(courseDTO.id() != null) {
+      course.setId(courseDTO.id());
+    }
+    course.setName(courseDTO.name());
+    course.setCategory(courseDTO.category());
+    course.setStatus("Ativo");
+    return course;
+  }
+
+}
