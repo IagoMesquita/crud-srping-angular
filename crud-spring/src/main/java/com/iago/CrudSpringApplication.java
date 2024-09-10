4eb0b40bc44ee2db2b3476e4c0cd9b07bfbb5e1f
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-10 15:47:57 -0300

    feat: add CommandLineRunner for inital database

--- crud-spring/src/main/java/com/iago/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/iago/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.iago;
 
+import com.iago.enums.Category;
+import com.iago.model.Course;
+import com.iago.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,24 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> extracted(courseRepository);
+	}
+
+	private void extracted(CourseRepository courseRepository) {
+		courseRepository.deleteAll();
+
+		for (int i = 1; i < 5; i++) {
+			Course course = new Course();
+
+			course.setName("Course " + i );
+			course.setCategory(Category.FRONT_END);
+			course.setStatus("Ativo");
+
+			courseRepository.save(course);
+		}
+
+	}
 }
