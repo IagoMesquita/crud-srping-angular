e4ffbcffd2abe7083bfb6dbf1170e561c8459cc2
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-10 16:45:19 -0300

    add: category converter with AttributeConverter

---
+++ crud-spring/src/main/java/com/iago/enums/converters/CategoryConverter.java
@@ -0,0 +1,31 @@
+package com.iago.enums.converters;
+
+import com.iago.enums.Category;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.Arrays;
+
+@Converter(autoApply = true) // --> jpa aplicar essa conversao sempre que necessario
+public class CategoryConverter implements AttributeConverter<Category, String> {
+
+  @Override
+  public String convertToDatabaseColumn(Category category) {
+    if (category == null) { // — > Ja possuem várias validacoes, mas aqui e apenas um reforco
+      return null;
+    }
+
+    return category.getValue();
+  }
+
+  @Override
+  public Category convertToEntityAttribute(String categoryValue) {
+    if (categoryValue == null) {
+      return null;
+    }
+
+    return Arrays.stream(Category.values())
+        .filter(category -> category.getValue().equals(categoryValue))
+        .findFirst()
+        .orElseThrow(IllegalArgumentException::new);
+  }
+}
