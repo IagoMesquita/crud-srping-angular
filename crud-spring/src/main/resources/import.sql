cb2923e11f5e0bd54316c93cd9455b7c744a5a12
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-10 15:48:18 -0300

    refactot: comment import.sql

--- crud-spring/src/main/resources/import.sql
+++ crud-spring/src/main/resources/import.sql
@@ -1,5 +1,5 @@
--- Query: SELECT * FROM a
--- exported data
-INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Flutter','Front-end','Ativo');
-INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Java','Back-end', 'Ativo');
-INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Spring','Back-end','Ativo');
+---- Query: SELECT * FROM a
+---- exported data
+--INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Flutter','Front-end','Ativo');
+--INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Java','Back-end', 'Ativo');
+--INSERT INTO `courses` (`name`,`category`,`status`) VALUES ('Spring','Back-end','Ativo');
