7a050894393915aef179e0f0b4c4c44462fa4089
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-10 15:47:05 -0300

    feat: add enum Category

---
+++ crud-spring/src/main/java/com/iago/enums/Category.java
@@ -0,0 +1,23 @@
+package com.iago.enums;
+
+public enum Category {
+
+  BACK_END("Back-end"), FRONT_END("Front-end");
+
+  private final String categoryValue;
+
+  private Category(String categoryValue) {
+    this.categoryValue = categoryValue;
+  }
+
+  public String getValue() {
+    return categoryValue;
+  }
+
+  @Override
+  public String toString() {
+    return "Category{" +
+        "categoryValue='" + categoryValue + '\'' +
+        '}';
+  }
+}
