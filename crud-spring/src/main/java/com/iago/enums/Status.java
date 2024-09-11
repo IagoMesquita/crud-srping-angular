f47a6f624e1240afc9e001e593a70b080016c9d5
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-11 11:14:11 -0300

    feat: add enum to Status

---
+++ crud-spring/src/main/java/com/iago/enums/Status.java
@@ -0,0 +1,17 @@
+package com.iago.enums;
+
+public enum Status {
+  ATIVO("Ativo"), INATIVO("Inativo");
+
+  private final String statusValue;
+
+  Status(String statusValue) {
+    this.statusValue = statusValue;
+  }
+
+  public String getStatusValue() {
+    return statusValue;
+  }
+
+
+}
