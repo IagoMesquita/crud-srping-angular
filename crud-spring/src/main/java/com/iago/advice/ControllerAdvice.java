e4e676448c7fb1fdc29a451d8655f809c791a226
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-14 13:13:46 -0300

    feat: add exception Handler MethodArgumentNotValidException

--- crud-spring/src/main/java/com/iago/advice/ControllerAdvice.java
+++ crud-spring/src/main/java/com/iago/advice/ControllerAdvice.java
@@ -2,6 +2,7 @@ package com.iago.advice;
 
 import com.iago.exception.RecordNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler(RecordNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(RecordNotFoundException e) {
-      return e.getMessage();
-    }
+  @ExceptionHandler(RecordNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handleNotFoundException(RecordNotFoundException e) {
+    return e.getMessage();
+  }
 
-
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    return e.getBindingResult().getFieldErrors()
+        .stream().map(error -> error.getField() + " " + error.getDefaultMessage())
+        .reduce("", (acc, error) -> acc + error + "\n");
+  }
 }
