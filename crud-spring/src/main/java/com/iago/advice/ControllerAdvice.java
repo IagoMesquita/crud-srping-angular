951775aecb385c700d831c1c29ca953d26904fc4
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-14 17:22:14 -0300

    feat: add more two exception Handler

--- crud-spring/src/main/java/com/iago/advice/ControllerAdvice.java
+++ crud-spring/src/main/java/com/iago/advice/ControllerAdvice.java
@@ -1,11 +1,14 @@
 package com.iago.advice;
 
 import com.iago.exception.RecordNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import java.util.Objects;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -23,4 +26,29 @@ public class ControllerAdvice {
         .stream().map(error -> error.getField() + " " + error.getDefaultMessage())
         .reduce("", (acc, error) -> acc + error + "\n");
   }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleConstraintViolationException(ConstraintViolationException e) {
+    return e.getConstraintViolations()
+        .stream().map(error -> error.getPropertyPath() + " " + error.getMessage())
+        .reduce("", (acc, error) -> acc + error + "\n");
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    if(e != null && e.getRequiredType() != null) {
+      String type = e.getRequiredType().getName();
+      // java.lang.Long
+      String[] typeParts = type.split("\\.");
+      //Long
+      String typeName = typeParts[typeParts.length -1];
+
+      return e.getName() + "  deve ser do tipo " + typeName;
+
+    }
+    return " Argument type not valid";
+  }
+
 }
