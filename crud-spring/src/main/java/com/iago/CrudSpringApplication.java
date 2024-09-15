e45a1c1dde42ccbfb659635476f0c01382615c76
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-09-15 13:49:05 -0300

    refactor: seeds

--- crud-spring/src/main/java/com/iago/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/iago/CrudSpringApplication.java
@@ -20,50 +20,56 @@ public class CrudSpringApplication {
 
   @Bean
   CommandLineRunner initDatabase(CourseRepository courseRepository) {
-//		return args -> extracted(courseRepository);
-    return args -> {
-			courseRepository.deleteAll();
-
-			Course course = new Course();
-			course.setName("JAVA");
-			course.setCategory(Category.BACK_END);
-			course.setStatus(Status.ATIVO);
-
-
-			Lesson lesson1 = new Lesson();
-			lesson1.setName("Aula 1: Fundamentos");
-			lesson1.setYoutubeUrl("1fd654fhd9d");
-			lesson1.setCourse(course);
-			course.getLessons().add(lesson1);
-
-			Lesson lesson2 = new Lesson();
-			lesson2.setName("Aula 2: OOP");
-			lesson2.setYoutubeUrl("1fdsfgj98j");
-			lesson2.setCourse(course);
-			course.getLessons().add(lesson2);
-
-			courseRepository.save(course);
-		};
+		return args -> extracted(courseRepository);
+//    return args -> {
+//			courseRepository.deleteAll();
+//
+//			Course course = new Course();
+//			course.setName("JAVA");
+//			course.setCategory(Category.BACK_END);
+//			course.setStatus(Status.ATIVO);
+//
+//
+//			Lesson lesson1 = new Lesson();
+//			lesson1.setName("Aula 1: Fundamentos");
+//			lesson1.setYoutubeUrl("1fd654fhd9d");
+//			lesson1.setCourse(course);
+//			course.getLessons().add(lesson1);
+//
+//			Lesson lesson2 = new Lesson();
+//			lesson2.setName("Aula 2: OOP");
+//			lesson2.setYoutubeUrl("1fdsfgj98j");
+//			lesson2.setCourse(course);
+//			course.getLessons().add(lesson2);
+//
+//			courseRepository.save(course);
+//		};
 
   }
 
-//  private void extracted(CourseRepository courseRepository) {
-//    courseRepository.deleteAll();
-//
-//    for (int i = 1; i < 5; i++) {
-//      Course course = new Course();
-//
-//      course.setName("Course " + i);
-//      course.setCategory(Category.BACK_END);
-//      course.setStatus(Status.ATIVO);
-//
-//      courseRepository.save(course);
-//    }
-//
-//    Lesson lesson = new Lesson();
-//    lesson.setName("javinha avancado");
-//    lesson.setYoutubeUrl("1fd654f");
-//
-//
-//  }
+  private void extracted(CourseRepository courseRepository) {
+    courseRepository.deleteAll();
+
+    for (int i = 1; i < 20; i++) {
+      Course course = new Course();
+
+      course.setName("Course " + i);
+      course.setCategory(Category.BACK_END);
+      course.setStatus(Status.ATIVO);
+
+      Lesson lesson = new Lesson();
+      lesson.setName("javinha avancado" + i);
+      lesson.setYoutubeUrl("1fd654f5h7");
+			lesson.setCourse(course);
+      course.getLessons().add(lesson);
+
+      courseRepository.save(course);
+    }
+
+    Lesson lesson = new Lesson();
+    lesson.setName("javinha avancado");
+    lesson.setYoutubeUrl("1fd654f5h7");
+
+
+  }
 }
